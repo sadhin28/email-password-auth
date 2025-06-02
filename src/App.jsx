42b8d051bb5c0e4c244f694185895c4de19ac25e
sadhin28 <taosifsadhin113@gmail.com>
2025-06-03 01:07:49 +0600

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-center font-bold '>Email Password Auth</h1>
-      <Outlet></Outlet>
+      <Navbar></Navbar>
+      <div className='min-h-[calc(100vh-50px)]'>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </>
   )
 }
